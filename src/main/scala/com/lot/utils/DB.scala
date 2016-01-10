aa4b7a589ae8798495203eb6a94bc7e05d1c422f
Mohith Thimmaiah <thimmaiah@gmail.com>
2016-01-10 11:29:02 +0530

    Config made env specific

--- src/main/scala/com/lot/utils/DB.scala
+++ src/main/scala/com/lot/utils/DB.scala
@@ -3,11 +3,21 @@ package com.lot.utils
 import slick.backend.DatabaseConfig
 import slick.driver.JdbcProfile
 
-object DB {
+object DB extends ConfigurationModuleImpl {
+  /**
+   * the env dev,test,prod we are operating in
+   */
+  val env = config.getString("env")
   
-	private val dbConfig : DatabaseConfig[JdbcProfile]  = DatabaseConfig.forConfig("lot_dev")
+  /**
+   * load the db config for this env
+   */
+	private val dbConfig : DatabaseConfig[JdbcProfile]  = DatabaseConfig.forConfig(s"lot_$env")
 	val profile: JdbcProfile = dbConfig.driver
 	
+	/**
+	 * This is the variable used by all consumers of the DB
+	 */
 	val db: JdbcProfile#Backend#Database = dbConfig.db
 
 }
